be48f64be419e64b11dedfa2fdb136de10c342aa
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-06-07 13:56:39 -0300

    "Método POST integrado com Front-End"

--- src/main/java/com/paulo/crudspring/controller/CourseController.java
+++ src/main/java/com/paulo/crudspring/controller/CourseController.java
@@ -3,10 +3,9 @@ package com.paulo.crudspring.controller;
 import com.paulo.crudspring.model.Course;
 import com.paulo.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +23,12 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        //Com ResponseEntity
+        //return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+
+       return courseRepository.save(course);
+    }
 }
