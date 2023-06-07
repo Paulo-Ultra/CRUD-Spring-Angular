534e23cec5592444f328284c9d0d2d3a31f304d2
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-06-07 13:57:19 -0300

    "Método POST integrado com Front-End"

--- src/main/java/com/paulo/crudspring/controller/CourseController.java
+++ src/main/java/com/paulo/crudspring/controller/CourseController.java
@@ -4,7 +4,6 @@ import com.paulo.crudspring.model.Course;
 import com.paulo.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
