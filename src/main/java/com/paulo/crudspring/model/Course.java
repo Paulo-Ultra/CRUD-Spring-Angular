32cf5f5281d88e01fc5e556969a0d781b92a401f
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-07-05 11:08:22 -0300

    Soft Delete com anotações @SQLDelete e @Where

--- src/main/java/com/paulo/crudspring/model/Course.java
+++ src/main/java/com/paulo/crudspring/model/Course.java
@@ -7,12 +7,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
 //Para mapear caso o nome da tabela seja diferente da criada aqui
 //@Table(name = "cursos")
+@SQLDelete(sql = "UPDATE Course SET status = 'Inactive' WHERE id = ?")
+@Where(clause = "status = 'Active'")
 public class Course {
 
     @Id
@@ -31,5 +35,11 @@ public class Course {
     @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Active|Inactive")
+    @Column(length = 10, nullable = false)
+    private String status = "Active";
 
 }
