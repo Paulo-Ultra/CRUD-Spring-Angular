2abac7a0219b8a2ddad77d43a6166ebce527d513
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-07-11 13:37:37 -0300

    ControllerAdvice implementada, tratamento de exceção e refatoração controller e service

---
+++ src/main/java/com/paulo/crudspring/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.paulo.crudspring.controller;
+
+import com.paulo.crudspring.exception.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+//Classe que passará para todos RestControllers o que fazer com as exceptions
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RecordNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFoundException(RecordNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
--- src/main/java/com/paulo/crudspring/service/CourseService.java
+++ src/main/java/com/paulo/crudspring/service/CourseService.java
@@ -1,5 +1,6 @@
 package com.paulo.crudspring.service;
 
+import com.paulo.crudspring.exception.RecordNotFoundException;
 import com.paulo.crudspring.model.Course;
 import com.paulo.crudspring.repository.CourseRepository;
 import jakarta.validation.Valid;
@@ -7,11 +8,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.Optional;
 
 @Validated
 @Service
@@ -25,28 +23,33 @@ public class CourseService {
     public List<Course> list() {
         return courseRepository.findAll();
     }
-    public Optional<Course> findById(@NotNull @Positive Long id) {
-        return courseRepository.findById(id);
+
+    public Course findById(@NotNull @Positive Long id) {
+        return courseRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(id));
     }
 
     public Course create(@Valid Course course) {
         return courseRepository.save(course);
     }
 
-    public Optional<Course> update(@NotNull @Positive Long id, @Valid Course course) {
+    public Course update(@NotNull @Positive Long id, @Valid Course course) {
         return courseRepository.findById(id)
                 .map(recordFound -> {
                     recordFound.setName(course.getName());
                     recordFound.setCategory(course.getCategory());
                     return courseRepository.save(recordFound);
-                });
+                }).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
-    public boolean delete(@NotNull @Positive Long id) {
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    courseRepository.deleteById(id);
-                    return true;
-                }).orElse(false);
+    public void delete(@NotNull @Positive Long id) {
+        courseRepository.delete(courseRepository.findById(id)
+                .orElseThrow(()-> new RecordNotFoundException(id)));
+
+//        courseRepository.findById(id)
+//                .map(recordFound -> {
+//                    courseRepository.deleteById(id);
+//                    return true;
+//                }).orElseThrow(() -> new RecordNotFoundException(id));
     }
 }
